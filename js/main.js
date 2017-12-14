faa720ddf1550633f085820e1429a0b00d5f0da5
Neil Brommer <neil@neilbrommer.com>
2017-12-13 19:51:20 -0800

    Better management of background images

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // use this when making changes so that there is no need to parse the page
 var bookmarkList;
 var dbVersion;
+var backgroundList = ["img/1.png", "img/2.png", "img/3.png", "img/4.png",
+	"img/5.png", "img/6.png", "img/7.png", "img/8.png"];
 
 $(document).ready(function () {
 	calcBackground();
@@ -16,26 +18,26 @@ function calcBackground() {
 	// 24*60 = 1440
 	// 2:40 between each step
 	if (total > 300 && total < 460) // 5:00 - 7:40
-		setBackground(1, false);
+		setBackground(0, false);
 	else if (total > 460 && total < 620)
-		setBackground(2, false);
+		setBackground(1, false);
 	else if (total > 620 && total < 780)
-		setBackground(3, false);
+		setBackground(2, false);
 	else if (total > 780 && total < 940)
-		setBackground(4, false);
+		setBackground(3, false);
 	else if (total > 940 && total < 1100)
-		setBackground(5, false);
+		setBackground(4, false);
 	else if (total > 1100 && total < 1260)
-		setBackground(6, true);
+		setBackground(5, true);
 	else if (total > 1260 && total < 1420)
-		setBackground(7, true);
+		setBackground(6, true);
 	else
-		setBackground(8, true);
+		setBackground(7, true);
 }
 
 function setBackground(num, dark) {
-	$("body").css("background-image", "url(img/" + num + ".png)");
 	// dark is a boolean that indicates the brightness of the background
+	$("body").css("background-image", "url(" + backgroundList[num] + ")");
 
 	if (dark) {
 		$(".navbar").removeClass("navbar-dark").addClass("navbar-light");
